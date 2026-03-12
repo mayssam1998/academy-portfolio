ee93df69362224b0aa8fefd7130e98d23a6843aa
nancyajouz23 <nancyajouz23@gmail.com>
2026-03-12 12:50:33 +0200

    project service

--- class-work/portfolio-project/src/app/interfaces/project.ts
+++ class-work/portfolio-project/src/app/interfaces/project.ts
@@ -1,1 +1,13 @@
-export interface Project {}
+export interface IProject {
+  id: number;
+  title: string;
+  image: string;
+  githubUrl: string;
+  liveUrl: string;
+  categoryId: number;
+}
+
+export interface ICategory {
+  id: number;
+  name: string;
+}

cf36bd125fbc09fe42f46d7200a9253173b5bfda
Evita Kleib <evit.kleib@gmail.com>
2026-03-14 19:08:56 +0200

    updated home component

--- class-work/project1/features/home/home.component.ts
+++ class-work/project1/features/home/home.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
-  standalone: true
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  openCV() {
+    window.open('assets/resume-example.pdf', '_blank');
+  }
 
+  goToContact() {
+    window.location.href = 'mailto:yourmail@example.com';
+  }
+
+  openLinkedIn() {
+    window.open('https://linkedin.com/', '_blank');
+  }
+
+  openGithub() {
+    window.open('https://github.com/', '_blank');
+  }
 }

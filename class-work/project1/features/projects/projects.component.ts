9a556ca9de9fa1b7a18313dafc6d3e339de75bf6
Evita Kleib <evit.kleib@gmail.com>
2026-03-14 19:10:25 +0200

    updated projects component

--- class-work/project1/features/projects/projects.component.ts
+++ class-work/project1/features/projects/projects.component.ts
@@ -2,10 +2,33 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  standalone: true,
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects = [
+    {
+      title: 'Project One',
+      image: 'assets/project-1.png',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      title: 'Project Two',
+      image: 'assets/project-2.png',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      title: 'Project Three',
+      image: 'assets/project-3.png',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    }
+  ];
 
+  openLink(url: string) {
+    window.open(url, '_blank');
+  }
 }

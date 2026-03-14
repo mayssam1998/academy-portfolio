f19ac5858361fbf3d41f459590410352fca68f03
Evita Kleib <evit.kleib@gmail.com>
2026-03-14 19:09:50 +0200

    updated about component

--- class-work/project1/features/about/about.component.ts
+++ class-work/project1/features/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })

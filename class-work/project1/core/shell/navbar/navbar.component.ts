a22c8a8ff3c475662df2d05ac97d3ae9165457ee
Evita Kleib <evit.kleib@gmail.com>
2026-03-14 19:07:52 +0200

    updated navbar

--- class-work/project1/core/shell/navbar/navbar.component.ts
+++ class-work/project1/core/shell/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
+  standalone: true,
   imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
